93a59dbbb24e3b9fcb8e3812a7c968e4f72c9514
kerem-yasir-ozyon <keremyasirozyon@gmail.com>
2024-07-21 23:26:02 +0300

    Atılmadan onceki son hali.

--- yemekTarifiProject/src/components/Anasayfa.jsx
+++ yemekTarifiProject/src/components/Anasayfa.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Head from './Head'
 import CardList from './CardList'
-import Footerr from './Footerr'
 
 const Anasayfa = () => {
   return (
     <div>
         <Head/> 
         <CardList/>
-        <Footerr/>
     </div>
   )
 }
